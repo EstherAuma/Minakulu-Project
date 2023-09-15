1dab03885bbabde4b372869b545ae31b36390c41
Esther Auma <estheraumaego@gmail.com>
2023-09-15 23:07:42 +0300

    New changes made

--- College/forms.py
+++ College/forms.py
@@ -14,3 +14,16 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Data
         fields = '__all__'
+
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update({'class': 'column1'})
+        self.fields['last_name'].widget.attrs.update({'class': 'column1'})
+        self.fields['gender'].widget.attrs.update({'class': 'column1'})
+        self.fields['occupation'].widget.attrs.update({'class': 'column1'})
+        self.fields['phone_number'].widget.attrs.update({'class': 'column1'})
+        self.fields['nationality'].widget.attrs.update({'class': 'column2'})
+        self.fields['district'].widget.attrs.update({'class': 'column2'})
+        self.fields['tribe'].widget.attrs.update({'class': 'column2'})
+        self.fields['email'].widget.attrs.update({'class': 'column2'})
--- College/models.py
+++ College/models.py
@@ -14,11 +14,13 @@ class User(models.Model):
 class Data(models.Model):
     first_name = models.CharField(max_length=20, blank=False, null=False)
     last_name = models.CharField(max_length=20, blank=False, null=False)
+    gender = models.CharField(max_length=10, blank=False, null=False)
     occupation = models.CharField(max_length=20, blank=False, null=False)
     phone_number = models.IntegerField(default=0)
     nationality = models.CharField(max_length=20, blank=False, null=False)
     district = models.CharField(max_length=20, blank=False, null=False)
     tribe = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(max_length=255)
 
     def __str__(self):
         self.first_name
